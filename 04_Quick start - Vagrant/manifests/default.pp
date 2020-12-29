3d2023fe722da68735436d10971b8d91e9ca47ea
Santhosh Ramesh <santhoshbalaji.ramesh@dieffenbacher.de>
2020-12-29 05:45:20 +0100

    update puppet and vagrant file

--- 04_Quick start - Vagrant/manifests/default.pp
+++ 04_Quick start - Vagrant/manifests/default.pp
@@ -1,36 +1,36 @@
 #execute apt-get update
-exec { 'apt-get':
-	command => '/usr/bin/apt-get update'
+exec { 'apt-get update':
+  command => '/usr/bin/apt-get update'
 }
 
 #install mongodb package
 package { 'mongodb':
-	require => Exce['apt-update'],
-	ensure => installed,
+  ensure  => installed,
+  require => Exec['apt-get update']
 }
 
 #run the service
 service { 'mongodb':
-	ensure => running,
-	require => Package['mongodb'],
+  ensure  => running,
+  require => Package['mongodb']
 }
 
 #install redis-server package
 package { 'redis-server':
-	require => Exce['apt-update'],
-	ensure => latest,
+  ensure  => latest,
+  require => Exec['apt-get update']
 }
 
 #run the service
 service { 'redis-server':
-	ensure => running,
-	require => Package['redis-server'],
+  ensure  => running,
+  require => Package['redis-server']
 }
 
 #create directory
-file { "/home/vagrant/my-projects"
-	ensure => "directory",
-	owner => "vagrant",
-	group => "vagrant",
-	mode => 750,
+file { '/home/vagrant/my-projects':
+  ensure => 'directory',
+  owner  => 'vagrant',
+  group  => 'vagrant',
+  mode   => '0750'
 }
