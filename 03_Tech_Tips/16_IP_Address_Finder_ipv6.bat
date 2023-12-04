e4a24c67d88bde0f9e444eab24dc6bb9518dd436
santhosh vm <santhoshbalaji.ramesh@dieffenbacher.de>
2023-12-04 23:10:59 +0100

    initial commit

--- 03_Tech_Tips/16_IP_Address_Finder_ipv6.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-echo IPv6
-echo ----
-for /f "delims=[] tokens=2" %%a in ('ping %computername% -6 -n 1 ^| findstr "["') do echo IP Address: %%a
-echo.
-echo ---------------------------------------------------
-echo.
-pause
