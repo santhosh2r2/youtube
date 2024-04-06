4f6ffc32efe9040b0f70588d82dd0b1f1033a4be
Santhosh Balaji Ramesh <santhosh.mwpa@gmail.com>
2024-04-07 01:47:36 +0200

    first draft version with astro

---
+++ astro.config.mjs
@@ -0,0 +1,47 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+import starlightBlog from 'starlight-blog'
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://santhosh2r2.github.io',
+  base: 'youtube',
+  integrations: [
+    starlight({
+      title: 'Home',
+      logo: {
+        src: "./src/assets/pic.jpg",
+        // replacesTitle: true,
+      },
+      social: {
+        github: 'https://github.com/santhosh2r2/youtube'
+      },
+      sidebar: [
+        {
+          label: 'Installation',
+          autogenerate: {
+            directory: 'install'
+          }
+        },
+        {
+          label: 'Blog',
+          autogenerate: {
+            directory: 'blog'
+          }
+        },
+      ],
+      plugins: [
+        starlightBlog({
+          authors: {
+            sramesh: {
+              name: "Santhosh Balaji Ramesh",
+              title: "Software Engineer",
+              url: "https://www.youtube.com/channel/UCR_Fuegjqal0Fvy6En2Bs3Q",
+              picture: "https://github.com/santhosh2r2.png"
+            }
+          }
+        })
+      ]
+    }),
+  ]
+});
