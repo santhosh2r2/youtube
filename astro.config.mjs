b1474a58a369dfe0f86d4fb2d4cc5ddd2d683492
Santhosh Balaji Ramesh <santhosh.mwpa@gmail.com>
2026-02-08 15:00:18 +0100

    [astro-config] update the sitemap version to prevent crash

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,17 @@
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
-import starlightBlog from 'starlight-blog';
-import starlightImageZoom from 'starlight-image-zoom';
+import { defineConfig } from "astro/config";
+import starlight from "@astrojs/starlight";
+import starlightBlog from "starlight-blog";
+import starlightImageZoom from "starlight-image-zoom";
 import AstroPWA from "@vite-pwa/astro";
 import manifest from "./webmanifest.json";
 
 import rehypeMermaid from "rehype-mermaid";
-import sitemap from '@astrojs/sitemap';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   vite: {
-    logLevel: 'info',
+    logLevel: "info",
     define: {
       __DATE__: `'${new Date().toISOString()}'`,
     },
@@ -20,9 +20,9 @@ export default defineConfig({
     outDir: "../dist/youtube",
     chunkSizeWarningLimit: 5000,
   },
-  site: 'https://santhosh2r2.github.io',
-  base: 'youtube',
-
+  site: "https://santhosh2r2.github.io",
+  base: "youtube",
+  output: "static",
   integrations: [
     starlight({
       head: [
@@ -30,25 +30,26 @@ export default defineConfig({
           tag: "link",
           attrs: {
             href: "/youtube/manifest.webmanifest",
-            rel: "manifest"
-          }
+            rel: "manifest",
+          },
         },
       ],
-      title: 'Home',
+      title: "Home",
       logo: {
         src: "./src/assets/pic.jpg",
       },
       components: {
-        SiteTitle: './src/components/overrides/SiteTitle.astro',
-        TableOfContents: './src/components/overrides/TableOfContents.astro',
+        SiteTitle: "./src/components/overrides/SiteTitle.astro",
+        TableOfContents: "./src/components/overrides/TableOfContents.astro",
         MarkdownContent: "./src/components/overrides/MarkdownContent.astro",
         // Sidebar: './src/components/overrides/Sidebar.astro',
       },
       social: {
-        youtube: "https://www.youtube.com/channel/UCR_Fuegjqal0Fvy6En2Bs3Q?sub_confirmation=1",
+        youtube:
+          "https://www.youtube.com/channel/UCR_Fuegjqal0Fvy6En2Bs3Q?sub_confirmation=1",
         linkedin: "https://www.linkedin.com/in/santhosh-balaji-ramesh/",
-        github: 'https://github.com/santhosh2r2/youtube',
-        email: "mailto:santhosh.mwpa@gmail.com"
+        github: "https://github.com/santhosh2r2/youtube",
+        email: "mailto:santhosh.mwpa@gmail.com",
       },
       favicon: "/image/pic.jpg",
       lastUpdated: true,
@@ -63,16 +64,27 @@ export default defineConfig({
               name: "Santhosh Balaji Ramesh",
               title: "Tech aficionado",
               url: "https://www.youtube.com/channel/UCR_Fuegjqal0Fvy6En2Bs3Q?sub_confirmation=1",
-              picture: "https://github.com/santhosh2r2.png"
-            }
+              picture: "https://github.com/santhosh2r2.png",
+            },
           },
-
         }),
       ],
       sidebar: [
-        { collapsed: false, label: 'General', autogenerate: { directory: 'general', collapsed: true } },
-        { collapsed: true, label: 'Tutorials', autogenerate: { directory: 'tutorial', } },
-        { collapsed: true, label: 'Projects', autogenerate: { directory: 'projects', } },
+        {
+          collapsed: false,
+          label: "General",
+          autogenerate: { directory: "general", collapsed: true },
+        },
+        {
+          collapsed: true,
+          label: "Tutorials",
+          autogenerate: { directory: "tutorial" },
+        },
+        {
+          collapsed: true,
+          label: "Projects",
+          autogenerate: { directory: "projects" },
+        },
       ],
     }),
     // registerType: "prompt",
@@ -94,14 +106,10 @@ export default defineConfig({
       },
     }),
     sitemap({
-      filter: (page) => !page.includes('/admin'),
+      filter: (page) => !page.includes("/admin"),
     }),
   ],
   markdown: {
     rehypePlugins: [[rehypeMermaid, { strategy: "img-svg", dark: true }]],
   },
-  image: {
-    service: { entrypoint: 'astro/assets/services/noop' },
-  },
-
 });
